6f4842b01c7ad269c5b935499c532b931c87ff6c
Hubert <hubert.langier@gmail.com>
2019-03-22 09:43:11 +0100

    Add files via upload

--- src/AccesComponent/Login.tsx
+++ src/AccesComponent/Login.tsx
@@ -13,22 +13,24 @@ export default class Login extends React.Component<any, any>{
             password: '',
             roles: [{}],
             validLogin: '',
-            validEmail: '',
+            validPassword: '',
             valid: false,
             signin: false,
-            URL: window.location.protocol + '//' + window.location.hostname + ':' + window.location.port
+            URL: window.location.protocol + '//'
+             + window.location.hostname + ':'
+             + window.location.port
         }
     }
 
     public forms: any;
 
-    public validationsLogin = (ele: any) => {
+    public validationsLogin = (ele: number) => {
         if (ele >= 3) { this.setState({ validLogin: 'Your name is correct', valid: true }) }
         else { this.setState({ validLogin: 'Your name to Short', valid: false }) };
     }
-    public validationsEmail = (ele: any) => {
-        if (ele >= 9) { this.setState({ validEmail: 'Your password to short', valid: true }) }
-        else { this.setState({ validEmail: 'Your password is correct', valid: false }) };
+    public validationsPassword = (ele: number) => {
+        if (ele >= 9) { this.setState({ validPassword: 'Your password to short', valid: true }) }
+        else { this.setState({ validPassword: 'Your password is correct', valid: false }) };
     }
     public onChangeUsername = (e: any) => {
         this.setState({ username: e.target.value });
@@ -36,7 +38,7 @@ export default class Login extends React.Component<any, any>{
     }
     public onChangePassword = (e: any) => {
         this.setState({ password: e.target.value });
-        this.validationsEmail(this.state.password.length);
+        this.validationsPassword(this.state.password.length);
     }
     public submit = async (e: any) => {
         Cookies.set('user', this.state.username);
@@ -56,7 +58,7 @@ export default class Login extends React.Component<any, any>{
                 })
                 .then(data => { this.setState({ signin: true }); return data });
         })
-            .catch(e => { console.log(e.responseText); this.setState({ validstate: 'login or email is not correct', signin: false }) });
+            .catch(e => { console.log(e.responseText); this.setState({ validstate: 'login or password is not correct', signin: false }) });
 
     }
     public render() {
@@ -69,12 +71,13 @@ export default class Login extends React.Component<any, any>{
                         <input placeholder='Login or Name' name='username' onChange={this.onChangeUsername} />
                     </Form.Field>
                     <Form.Field>
-                        <span>{this.state.validEmail}</span>
+                        <span>{this.state.validPassword}</span>
                         <input placeholder='Password' name='password' onChange={this.onChangePassword} />
                     </Form.Field>
                     <Button fluid color='green'><Icon name='user secret' />Login</Button>
                 </form>
-                {this.state.signin === true && this.state.valid === true && systemTool.ifLoged() ? <Redirect to="/user" /> : null}
+                {this.state.signin === true && this.state.valid === true && systemTool.ifLoged() ? 
+                <Redirect to="/user" /> : this.state.validstate}
             </Segment>
         );
     }
